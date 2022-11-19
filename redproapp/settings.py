a5eb50bad91bdb7d8fc7036df6224e11ff67569c
“lasagna0” <dsanchezpolo@icloud.com>
2022-11-19 18:51:18 -0500

    Automatic configuration with django-heroku

--- redproapp/settings.py
+++ redproapp/settings.py
@@ -131,3 +131,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
