fc7f8865987c1d88b128f628b3e957ab09378f7c
David Alejandro Sanchez Polo <65139707+lasagna0@users.noreply.github.com>
2022-11-22 12:43:50 -0500

    STATIC

--- redproapp/settings.py
+++ redproapp/settings.py
@@ -135,8 +135,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, '..', 'static'),
+)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Default primary key field type
+
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
