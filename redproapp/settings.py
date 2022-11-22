f16ea84205a6482b842645e0686020c28d2f8291
David Alejandro Sanchez Polo <65139707+lasagna0@users.noreply.github.com>
2022-11-22 12:31:48 -0500

    staticfixxxxx

--- redproapp/settings.py
+++ redproapp/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "False"
 
-ALLOWED_HOSTS = ["plankton-app-2-jjnyd.ondigitalocean.app"]
+ALLOWED_HOSTS = ["plankton-app-2-jjnyd.ondigitalocean.app", "127.0.0.1"]
 
 
 LOGIN_REDIRECT_URL = "/"
@@ -136,8 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
